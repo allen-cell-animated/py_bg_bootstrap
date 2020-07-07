6c062e259b4c46e31198f3ffdd817fcc1d552f8e
Jamie Sherman <jamie.sherman@gmail.com>
2020-07-07 12:05:25 -0700

    Feature/background update (#1)
    * preliminary working prototype
    
    * removing some example templates.
    
    * corrected remote repo urls etc
    
    * updated readme and test
    
    * update readme

---
+++ py_bg_bootstrap/tests/test_bg_boot.py
@@ -0,0 +1,11 @@
+from py_bg_bootstrap import Bootstrapper
+
+
+def test_dist(poisson_array):
+    parr = poisson_array
+    bst = Bootstrapper(bg_imgs=parr, division=1)
+    cut_offs = bst.compute_confidence(threshold=95)
+    assert cut_offs[0, 0] == 4.0
+
+
+
