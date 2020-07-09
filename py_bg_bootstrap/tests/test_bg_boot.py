82e185657711644c1139136a130165356be60c16
heeler <jamie.sherman@gmail.com>
2020-07-08 23:34:02 -0700

    fixed threshold test

--- py_bg_bootstrap/tests/test_bg_boot.py
+++ py_bg_bootstrap/tests/test_bg_boot.py
@@ -5,7 +5,7 @@ def test_dist(poisson_array):
     parr = poisson_array
     bst = Bootstrapper(bg_imgs=parr, division=1)
     cut_offs = bst.compute_confidence(threshold=95)
-    assert cut_offs[0, 0] == 4.0
+    assert cut_offs[0, 0] == 5.0
 
 
 
